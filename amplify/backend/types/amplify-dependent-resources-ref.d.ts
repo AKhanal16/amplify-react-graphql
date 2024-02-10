a26c754a6dd6e4013cfa82c9044b72c7aab1329d
Abhishek <abhishekkhanal16@gmail.com>
2024-02-10 18:02:54 -0500

    Added authentication, api, and database

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqld8e94f75": {
       "AppClientID": "string",
